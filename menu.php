4a018664697767fd7c55998629a0f26c7ef9c64d
gisito <62222322+gisito@users.noreply.github.com>
2020-03-15 23:16:53 +0000

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<categories>
+<category clave="0" type="config" title="CONFIGURACION" description="" sd_img="http://38.18.239.240/descargas/images/bb/setting2.jpg" hd_img="http://38.18.239.240/descargas/images/bb/buscadorx.jpg">
+</category>
+
+<!--- 
+
+<category clave="0" type="avisos" title="NOTIFICACIONES" description="" sd_img="http://38.18.239.240/descargas/images/bb/xnotifica1.jpg" hd_img="http://38.18.239.240/descargas/images/bb/buscadorx.jpg">
+</category>
+
+-->
+
+<category clave="0" type="buscador" title="SEARCH" description="" sd_img="http://38.18.239.240/descargas/images/bb/buscadorx.jpg" hd_img="http://38.18.239.240/descargas/images/bb/buscadorx.jpg">
+</category>
+<category clave="6" type="television" title="TV" description="" sd_img="http://38.18.239.240/descargas/images/bb/tvenvivox.jpg" hd_img="http://38.18.239.240/descargas/images/bb/tvenvivox.jpg">
+</category>
+<category clave="2" type="cuadros" title="SERIES" description="" sd_img="http://38.18.239.240/descargas/images/bb/seriesx.jpg" hd_img="http://38.18.239.240/descargas/images/bb/seriesx.jpg">
+</category>
+<category clave="1" type="cuadros" title="MOVIES" description="" sd_img="http://38.18.239.240/descargas/images/bb/peliculasx.jpg" hd_img="http://38.18.239.240/descargas/images/bb/peliculasx.jpg">
+</category>
+
+<!--- 
+
+<category clave="9" type="cuadros" title="THE WALKING DEAD" description="" sd_img="http://38.18.239.240/descargas/images/bb/walking.jpg" hd_img="http://38.18.239.240/descargas/images/bb/walking.jpg">
+</category>
+
+<category clave="14" type="cuadros" title="ESPECIAL DIA DE MUERTOS" description="" sd_img="http://38.18.239.240/descargas/images/bb/muertos.jpg" hd_img="http://38.18.239.240/descargas/images/bb/muertos.jpg">
+</category>
+
+-->
+
+
+<category clave="3" type="cuadros" title="KIDS ZONE" description="" sd_img="http://38.18.239.240/descargas/images/bb/kidsx.jpg" hd_img="http://38.18.239.240/descargas/images/bb/kidsx.jpg">
+</category>
+
+<category clave="13" type="cuadros" title="KARAOKE" description="" sd_img="http://38.18.239.240/descargas/images/bb/karaokex.jpg" hd_img="http://38.18.239.240/descargas/images/bb/karaokex.jpg">
+</category>
+
+<category clave="15" type="cuadros" title="CONCIERTOS" description="" sd_img="http://38.18.239.240/descargas/images/bb/conciertosx.jpg" hd_img="http://38.18.239.240/descargas/images/bb/conciertosx.jpg">
+</category>
+
+
+<category  clave="5" type="special_category"  title="+18" description="" sd_img="http://38.18.239.240/descargas/images/bb/18x.jpg" hd_img="http://38.18.239.240/descargas/images/bb/18x.jpg">
+</category>
+</categories>
